1fd5494242758bc8912e9814b7789ba9a07e841d
Roman Lavrenov <roman.lavrenov@molex.com>
2024-02-01 16:41:22 -0500

    layout updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,64 +1,68 @@
-import '@code-hike/mdx/dist/index.css';
-import './app.css';
+import "@code-hike/mdx/dist/index.css";
+import "./app.css";
 
 function MyApp({ Component, pageProps }) {
-  const headerItems = [
-    'Getting Started',
-    'Libraries',
-    'CLI',
-    'Studio',
-    'Hosting',
-    'Guides',
-    'API Reference',
-  ];
-
+  const headerItems = {
+    left: ["Amplify Dev Center", "Docs", "Learn", "UI Library", "Contribute"],
+    right: ["About AWS Amplify", "Pricing"],
+  };
   const modulesNames = [
-    'Getting Started',
-    'Prerequisites',
-    'Set up fullstack',
-    'Connect API and database to the app',
-    'Add authentication',
-    'Deploy and host app',
-    'Next steps',
+    "Getting Started",
+    "Prerequisites",
+    "Set up fullstack",
+    "Connect API and database",
+    "Add authentication",
+    "Deploy and host app",
+    "Next steps",
   ];
 
   const quickLinks = [
-    'Set up fullstack project',
-    'Create a new React app',
-    'Initialize a new backend',
-    'Install Amplify Libraries',
-    'Set up frontend',
+    "Set up fullstack project",
+    "Create a new React app",
+    "Initialize a new backend",
+    "Install Amplify Libraries",
+    "Set up frontend",
   ];
 
   return (
     <>
       <header>
-        {headerItems.map((item) => {
-          return (
-            <div className='header_item' key={item}>
-              {item}
-            </div>
-          );
-        })}
-        <input type='search' id='site-search' placeholder='Search Docs'></input>
+        <div className="left">
+          {headerItems.left.map((item) => {
+            return (
+              <div className="header_item" key={item}>
+                {item}
+              </div>
+            );
+          })}
+        </div>
+        <div className="right">
+          {headerItems.right.map((item) => {
+            return (
+              <div className="header_item" key={item}>
+                {item}
+              </div>
+            );
+          })}
+        </div>
       </header>
-      <navbar className='modules'>
+      <navbar className="modules">
         {modulesNames.map((name) => {
           return (
-            <div className='side_item' key={name}>
+            <div className="side_item" key={name}>
               {name}
             </div>
           );
         })}
       </navbar>
-      <div className='codehike'>
-        <a href='/'>Home</a>
+      <div className="codehike">
+        <a href="/">Home</a>
         <Component {...pageProps} />
       </div>
-      <navbar className='quicklinks'>
+      <navbar className="quicklinks">
         {quickLinks.map((link) => {
           return (
-            <div className='side_item' key={link}>
+            <div className="side_item" key={link}>
               {link}
             </div>
           );
